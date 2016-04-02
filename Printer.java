a2b7d6ab40697f42c58cf221fdfe531321d7c96b
Joey Chan <joeyc.152@gmail.com>
2016-04-02 17:46:01 -0400

    fixed account type: int -> BankAccount
    account -> account.getAccountNumber()

--- Printer.java
+++ Printer.java
@@ -1,48 +1,49 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 
 public class Printer {
 	
-	private Transations transactions;
-	private int account;
+	private Transactions transactions;
+	private BankAccount account;
 	private String name;
 	private int[] type;
-	private int[] dates;
+	private String[] date;
 	private int[] balance;
 	private int[] amount;
 	
 	public Printer(Transactions t) {
 		transactions = t;
 		account = transactions.getAccount();
-		name = transactions.getName();
+		name = account.getName();
 		type = transactions.getTransactionType();
-		dates = transactions.getTransactionDate();
+		date = transactions.getTransactionDate();
 		balance = transactions.getTransactionBalance();
 		amount = transactions.getTransactionBalance();
 	}
 	
 	public void printReceipt() throws IOException {
-		String fileName = "Receipt - " + account + ".txt";
-		PrintWriter pw = new PrintWriter(new FileWriter(filename));
+		String fileName = "Receipt - " + account.getAccountNumber() + ".txt";
+		PrintWriter pw = new PrintWriter(new FileWriter(fileName));
 		
 		pw.println("Receipt");
 		pw.println("Name:\t" + name);
-		pw.println("Card Number:\t" + account);
+		pw.println("Card Number:\t" + account.getAccountNumber());
 		pw.println("Date:\t" + date[0]);
 		pw.println();
 		for(int i = 0; i < type.length; i++) {
-			String t;
+			String t = "";
 			switch(type[i]) {
 			case 1:
 				t = "Deposit";
 				break;
 			case 2:
-				t = "Withdraw"
+				t = "Withdraw";
 				break;
 			case 3:
-				t = "Donate"
+				t = "Donate";
 				break;
 			}
 			pw.println("Transaction:\t" + t);
@@ -54,9 +55,10 @@ public class Printer {
 	}
 	
 	public String printTransactionHistory() {
-		Scanner sc = new Scanner(account + ".txt");
-		String history;
-		history += sc.nextLine();
+		Scanner sc = new Scanner(account.getAccountNumber() + ".txt");
+		String history = "";
+		history = history + sc.nextLine() + " ";
+		sc.close();
 		return history;
 	}
 }
