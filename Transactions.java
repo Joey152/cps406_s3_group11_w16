28748af86eaee53d1ccdd3b91799f292af8f3a94
Joey Chan <joeyc.152@gmail.com>
2016-04-03 14:50:38 -0400

    Finished Transactions

--- Transactions.java
+++ Transactions.java
@@ -1,30 +1,80 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 
 public class Transactions {
 	private BankAccount account;
-	private int[] transactionType;
-	private String[] transactionDates;
-	private int[] transactionBalance;
-	private int[] transactionAmount;
+	private ArrayList<Integer> transactionType;
+	private ArrayList<String> transactionDates;
+	private ArrayList<Integer> transactionBalance;
+	private ArrayList<Integer> transactionAmount;
 	
 	public Transactions(BankAccount account) {
 		this.account = account;
+		transactionType = new ArrayList<>();
+		transactionDates = new ArrayList<>();
+		transactionBalance = new ArrayList<>();
+		transactionAmount = new ArrayList<>();
 	}
 	
+	/**
+	 * 
+	 * @param amount
+	 * @return 0 is success
+	 * @return 1 is not enough money in account
+	 * @return 2 is over/under the withdraw max/min
+	 */
 	public int withdraw(int amount) {
-		if(amount < account.minWithdraw && amount > account.maxWithdraw) {
+		int newBalance = account.getAccountBalance() - amount;
+		if(newBalance < 0) {
 			return 1;
 		}
-		else if(true) {
-			
-		}
+		if(amount < account.getMinWithdraw() && amount > account.getMaxWithdraw()) {
+			return 2;
+		}		
+		
+		account.setAccountBalance(newBalance);
+		transactionType.add(1);
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date dateobj = new Date();
+		transactionDates.add(df.format(dateobj));
+		transactionBalance.add(newBalance);
+		transactionAmount.add(amount);
 		return 0;
 	}
 	
 	public int deposit(int amount) {
+		int newBalance = account.getAccountBalance() + amount;
+		account.setAccountBalance(newBalance);
+		transactionType.add(1);
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date dateobj = new Date();
+		transactionDates.add(df.format(dateobj));
+		transactionBalance.add(newBalance);
+		transactionAmount.add(amount);
 		return 0;
 	}
 	
+	/**
+	 * Always donate $1, money is not another account yet
+	 * @param amount
+	 * @return 0 is success
+	 * @return 1 is not enough money in account 
+	 */
 	public int donate(int amount) {
+		int newBalance = account.getAccountBalance() - 1;
+		if(newBalance < 0) {
+			return 1;
+		}
+		account.setAccountBalance(newBalance);
+		transactionType.add(2);
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date dateobj = new Date();
+		transactionDates.add(df.format(dateobj));
+		transactionBalance.add(newBalance);
+		transactionAmount.add(amount);
 		return 0;
 	}
 	
@@ -32,19 +82,19 @@ public class Transactions {
 		return account;
 	}
 	
-	public int[] getTransactionType() {
+	public ArrayList<Integer> getTransactionType() {
 		return transactionType;
 	}
 	
-	public String[] getTransactionDate() {
+	public ArrayList<String> getTransactionDate() {
 		return transactionDates;
 	}
 	
-	public int[] getTransactionBalance() {
+	public ArrayList<Integer> getTransactionBalance() {
 		return transactionBalance;
 	}
 	
-	public int[] getTransactionAmount() {
+	public ArrayList<Integer> getTransactionAmount() {
 		return transactionAmount;
 	}
 }
